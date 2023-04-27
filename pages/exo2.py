3cc5c31912d9c3bc4735cef539349948d76afb8b
eliana cuhadar <eliana.cuhadar.69@hotmail.com>
2023-04-27 15:05:07 +0200

    Exo 2 underway, predictions on drawing should work

--- pages/exo2.py
+++ pages/exo2.py
@@ -3,17 +3,32 @@ import pandas as pd
 import numpy as np
 import pickle
 from streamlit_app import model_1, df_test
-from exo1 import make_pred
+
 from streamlit_drawable_canvas import st_canvas
+from PIL import Image
 
 st.title("Drawable Canvas")
 
-st.sidebar.header("Configuration")
-
-# Specify brush parameters and drawing mode
-b_width = st.sidebar.slider("Brush width: ", 1, 100, 10)
-drawing_mode = st.sidebar.checkbox("Drawing mode ?", True)
-
-image_data = st_canvas(
-    b_width, height=150, drawing_mode=drawing_mode, key="canvas"
+image_canv = st_canvas(
+     height=280,
+     width=280,
+     background_color  ="#000000",
+     stroke_color = "#FFFFFF"
 )
+
+img_resized = Image.fromarray(image_canv.image_data.astype('uint8')).resize((28, 28))
+# Convert the image to grayscale
+img_gray = img_resized.convert('L')
+# Convertir l'image en array numpy
+img_array = np.array(img_gray)
+# Traiter l'image comme nécessaire (ex: la normaliser)
+processed_img_array = img_array / 255.0
+st.image(processed_img_array)
+# Stocker l'image dans une variable
+image = np.expand_dims(processed_img_array, axis=0)
+
+def pred():
+    return model_1.predict(processed_img_array.reshape(1, 28, 28, 1)).argmax()
+
+if st.button("Pred"):
+    st.header(f'La prédiction est {pred()}')
