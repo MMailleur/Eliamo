bd4b9f4b391782b41dbeeba5e0ad839aaca0d510
morganmailleur <m.mailleur@gmail.com>
2023-04-28 12:19:30 +0200

    push exo 1

--- pages/exo1.py
+++ pages/exo1.py
@@ -3,9 +3,17 @@ import pandas as pd
 import numpy as np
 import pickle
 from streamlit_app import model_1, df_test
+import streamlit_nested_layout
+from threading import Timer
+import time
+from utils import yes_gif,sad_gif
 
 
-col1, col2,col3 = st.columns(3)
+if 'good' not in st.session_state:
+    st.session_state.good = False
+if 'bad' not in st.session_state:
+    st.session_state.bad = False
+col1, col2,col3 = st.columns([0.5,0.2,1])
 
 def make_pred():
     row_test = df_test.sample(1)
@@ -13,8 +21,7 @@ def make_pred():
     pred = model_1.predict(row_test.values.reshape(1, 28, 28, 1)).argmax()
 
     return row_test,arr,pred
-good = False
-bad = False
+
 row_test,arr,pred= make_pred()
 
 with col1:
@@ -22,13 +29,30 @@ with col1:
 
 
     st.image(arr,use_column_width = "always")
-    st.header(f'La prédiction est {pred}')
-    if st.button('Predict good ?'):
-        good = True
-    if st.button('Predict bad ?'):
-        bad = True
+
+
 
 with col3:
+    st.title(f'La prédiction est {pred}')
+    col22,col23=st.columns(2)
 
-    st.title('Pov moi')
-    st.markdown("![Alt Text](https://media.tenor.com/3HZaZH1XxRsAAAAC/le-coeur-a-ses-raisons-le-coeur-a-ses-raison.gif)")
+    with col22 :
+        if st.button('Predict juste ?'):
+            st.session_state.good = True
+        else :
+            st.session_state.good = False
+
+
+
+
+    with col23 :
+        if  st.button('Predict fausse ?'):
+            st.session_state.bad = True
+        else :
+            st.session_state.bad = False
+
+
+    if  st.session_state.bad :
+        st.markdown(f"![Alt Text]({sad_gif[np.random.randint(0,6)]})")
+    if  st.session_state.good :
+        st.markdown(f"![Alt Text]({yes_gif[np.random.randint(0,6)]})")
---
+++ utils.py
@@ -0,0 +1,20 @@
+yes_gif =[
+    "https://media.tenor.com/AzNJtG4GXToAAAAM/yes.gif",
+    "https://media.tenor.com/qdg13PqYbxMAAAAM/yes-baby.gif",
+    "https://media.tenor.com/2DVpsEpFaHQAAAAM/yes.gif",
+    "https://media.tenor.com/EzlQnqyFvmcAAAAM/yes-yesss-meme.gif"
+    "https://media.tenor.com/F-mq05c3FUIAAAAM/celebrate-confetti.gif",
+    "https://media.tenor.com/-WKAkdEbIUwAAAAM/celebrate-confetti.gif",
+    "https://media.tenor.com/bDnEIJQYf-8AAAAM/yesss-leona-lewis.gif"
+
+    ]
+
+sad_gif =[
+    "https://media.tenor.com/xjIYN3WkB4wAAAAM/smile-person-akirambow.gif",
+    "https://media.tenor.com/eorzo18pmJoAAAAM/cringe.gif",
+    "https://media.tenor.com/o9owbF_iALAAAAAM/sad-cat.gif",
+    "https://media.tenor.com/zwuefXVe4mEAAAAM/sad-face.gif"
+    "https://media.tenor.com/hmywIR2hsz0AAAAM/sad-depressed.gif",
+    "https://media.tenor.com/i3zBMIwupvUAAAAM/sad-face.gif",
+    "https://media.tenor.com/LanjdqF7wnsAAAAM/cry-sad.gif"
+    ]
